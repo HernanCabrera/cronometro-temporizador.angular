5a9a12033378bc832350e5663f6f874391568b0b
Hernan Cabrera <mauricioh_cabrera@hotmail.com>
2018-02-17 12:42:56 -0300

    Pipes conversor-a-tiempo + mejor semantica en los componentes hijos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,30 +6,41 @@ import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
 
+/*Rutas*/
 import { TemporizadorComponent } from './componentes/temporizador/temporizador.component';
 import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
 import { CronometroComponent } from './componentes/cronometro/cronometro.component';
 import { ManualComponent } from './componentes/manual/manual.component';
 
+/*Componentes*/
 import { TiempoComponent } from './componentes/hijo/tiempo/tiempo.component';
 import { TiempoBotonesComponent } from './componentes/hijo/tiempo botones/tiempo-botones.component';
 import { NuevoTiempoComponent } from './componentes/hijo/nuevo tiempo/nuevo-tiempo.component';
 import { ListaDeTiemposComponent } from './componentes/hijo/lista de tiempos/lista-de-tiempos.component';
 
+/*Vistas*/
 import { HeaderComponent } from './componentes/diseño/header/header.component';
+
+/*Pipes*/
+import { ConversorATiempoPipe } from './pipes/conversor-a-tiempo.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
+    /* Vistas */
     HeaderComponent,
+    /* Rutas */
     TemporizadorComponent,
     CronometroComponent,
     ManualComponent,
     ConfiguracionComponent,
+    /* Componentes */
     TiempoComponent,
     TiempoBotonesComponent,
     NuevoTiempoComponent,
-    ListaDeTiemposComponent
+    ListaDeTiemposComponent,
+    /* Pipes */
+    ConversorATiempoPipe
   ],
   imports: [
     BrowserModule,
